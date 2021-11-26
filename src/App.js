49d7da8568d0d1573b722f977ecf5c158ad3026d
KingMinyu <als_1009@naver.com>
2021-11-26 20:13:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src = {logo} lat = 'logo' />
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;

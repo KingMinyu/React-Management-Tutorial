a921a891806f95dd7b4e4e5f77e020d673c3b97d
KingMinyu <als_1009@naver.com>
2021-12-13 21:11:17 +0900

    clean push

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 
 ReactDOM.render(
   <React.StrictMode>
